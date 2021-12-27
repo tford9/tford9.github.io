565b1d9c32673e7dac4ec7a015ea005a37519eaa
Trenton - Linux Box <tford5@nd.edu>
2021-12-27 13:21:06 -0500

    adding eleventy support

---
+++ _11ty/filters/excerpt.js
@@ -0,0 +1,14 @@
+const cheerio = require('cheerio');
+
+module.exports = {
+
+  excerpt: (content, limit = 156) => {
+    if (content) {
+      let txt = content.slice(0, content.indexOf('<!--more-->'));
+      txt = cheerio.load(txt).text().replace(/\r\n|\n|\r|\t/g, ' ').trim().slice(0, limit);
+      txt = txt.slice(0, txt.lastIndexOf(' '));
+      return txt;
+    }
+  }
+
+};
